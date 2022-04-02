eb6089b9e431c1a62ffb1a3aedab01a7aab813e9
Chance <chancecrawford30@gmail.com>
2022-04-02 12:06:30 -0700

    added build to package.json for deployment

--- server/server.js
+++ server/server.js
@@ -64,11 +64,31 @@ app.use(express.json());
 
 
 // Serve up static assets
+// In production, the Express.js app will use the value of process.env.PORT instead of 3001. 
+// Heroku won't let us use Create React App's default port of 3000, though. Regardless, 
+// we wouldn't want to run nodemon or the Create React App server because neither codebase 
+// would be dynamically changing once deployed.
+// Ultimately, we only need a static Node.js server that serves the built 
+// (i.e., compiled) front-end files.
 // We just added two important pieces of code that will only come into effect 
 // when we go into production. First, we check to see if the Node environment 
-// is in production. If it is, we instruct the Express.js server to serve any 
+// is in production (at Heroku). If it is, we instruct the Express.js server to serve any 
 // files in the React application's build directory in the client folder.
 // remember this is for production only.
+// When deployed, the Express.js app will attempt to serve all static files in 
+// client/build. We just need to make sure those files exist on Heroku.
+// One thing we could do is build the files manually with the react-scripts build command 
+// and then commit the files with Git. But this approach could lead to unnecessary merge 
+// conflicts as other developers start working on the app and building the files themselves.
+// Alternatively, we could tell Heroku to build the front-end files whenever a push is made to 
+// production. This would guarantee that Heroku always has the latest version in case a 
+// developer ever forgets to manually build. Let's give this option a try.
+// We do this in the package.json with the "build" command.
+// Remember that Heroku will automatically look for the start command to run your app. 
+// Heroku will also try to run a build command if it can find it.
+// The build command in the package.json will tell Heroku to cd into the client 
+// folder after the initial install and build the React files for you, so that it will happen
+// automatically. 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
